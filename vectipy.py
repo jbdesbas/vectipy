333f834f7fa37277aa01c695a04e7a1b386ac3ab
JB Desbas <jb.desbas@gmail.com>
2021-03-27 22:41:49 +0100

    command

--- vectipy.py
+++ vectipy.py
@@ -1,9 +1,18 @@
 from app import create_app
 import os
+from flask.cli import FlaskGroup, click
+from flask import current_app
 
 app=create_app()
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5001))
-    app.run(debug=True, host='0.0.0.0', port=port)
+cli = FlaskGroup(create_app=create_app)
 
+from app.mvtserver import scandb
+import toml
+@cli.command('scan_db')
+def scan_db():
+    click.echo( toml.dumps(scandb(current_app.config['DB'])) )
+
+
+if __name__ == '__main__':
+    cli()
