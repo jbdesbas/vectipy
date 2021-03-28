0d7c8121242d91630d91e4c62940e9be9be8dd25
JB Desbas <jb.desbas@gmail.com>
2021-03-28 20:50:45 +0200

    todo

--- vectipy.py
+++ vectipy.py
@@ -11,8 +11,23 @@ from app.mvtserver import scandb
 import toml
 @cli.command('scan_db')
 def scan_db():
+    """Auto-generate layers definition (layers.toml) from database"""
     click.echo( toml.dumps(scandb(current_app.config['DB'])) )
 
+
+@cli.command('check')
+def check():
+    """Everything is fine ?"""
+    click.echo('DB connection?         ✅❌')
+    click.echo('Postgis version?       ✅❌')
+    click.echo('Test st_asMvt Func     ✅❌')
+    
+    click.echo('Layer Index ?          ✅⚠️')
+    click.echo('Layer SRID ?           ✅⚠️')
+    click.echo('Layer Geometry type ?  ✅⚠️')
+
+    click.echo('layers.toml            ✅❌')
+    pass
 
 if __name__ == '__main__':
     cli()
